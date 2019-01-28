e5be8da8b05e1c5c06adf0cd3510d93a071c85d5
zt <299757702@qq.com>
2019-01-28 20:00:54 +0800

    项目基本框架

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ console.log(new Date().toLocaleString());
 const PORT = 8090;
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const categoryRouter = require('./routes/admin/category');
+const adminRouter = require('./routes/admin/admin')
 
 //创建HTTP应用服务器
 var app=express();
@@ -15,6 +17,9 @@ app.listen(PORT,()=>{
 
 //使用中间件
 app.use(cors());
+//app.use(bodyParser.urlencoded())//把application/x-www-form-urlcoded格式的请求主体放到req.body中
+app.use(bodyParser.json())//把JSON格式的请求主体数据解析出来放入到req.body中,高效方便
 
 //挂载路由器
 app.use('/admin/categroy',categoryRouter)
+app.use('/admin',adminRouter)
--- routes/admin/category.js
+++ routes/admin/category.js
@@ -25,26 +25,44 @@ router.get('/',(req,res)=>{
 code:400,msg:'0 categroy deleted'
 */
 router.delete('/:cid',(req,res)=>{
-  
-  pool.query('DELETE FROM xfn_categroy WHERE cid=?',req.params.cid,(err,result)=>{
+  //删除菜品类别前必须先把属于该类别的菜品的类别编号改为NULL外键约束问题--先update,再delete
+  pool.query('UPDATE xfn_dish SET categoryId=NULL WHERE categoryId=?',req.params.cid,(err,result)=>{
+    //直接写SQL语句是异步请求，不会按先后执行，加上if就可以了
+
     if(err) throw err;
-    //获取delete语句在数据库中影响行数
-    if(result.affectedRows>0){
-      res.send({code:200,msg:'1 categroy deleted'})
-    }else{
-      res.send({code:400,msg:'0 categroy deleted'})
-    }
-    
+    //至此指定类别的菜品已经修改完毕
+
+    pool.query('DELETE FROM xfn_category WHERE cid=?',req.params.cid,(err,result)=>{
+      if(err) throw err;
+      //获取delete语句在数据库中影响行数
+      if(result.affectedRows>0){
+        res.send({code:200,msg:'1 categroy deleted'})
+      }else{
+        res.send({code:400,msg:'0 categroy deleted'})
+      }
+      
+    })
   })
+
+ 
 })
 
 
-/*API:POST/admin/category    |添加
+/*API:POST/admin/category    |添加  POST非幂等  PUT是幂等（添加相同的信息，不管执行 添加多少次，结果是一样的）
 *请求主体参数:{cname:'xxx'}  要求使用JSON格式，不使用键值对格式
 *含义：客户端获取所有的菜品类别，按编号升序排列
 *返回值形如：
 *[{code:200,msg:'1 categroy deleted'，cid:x}]
 */
+router.post('/',(req,res)=>{
+  console.log('获取到请求数据');
+  //console.log(req.body);
+  var data = req.body;//形如{cname:'xxx'}
+  pool.query('INSERT INTO xfn_category SET ?',data,(err,result)=>{
+    if(err) throw err;
+    res.send({code:200,msg:'1 category post'})
+  }) //insert 和update可简写set自动根据json来添加
+})
 
 
 /*API:PUT/admin/category    |更新
@@ -58,4 +76,26 @@ router.delete('/:cid',(req,res)=>{
 
 ]
 */
+router.put('/',(req,res)=>{
+  var data=req.body;//请求数据{cid:xx,cname:'xx'}
+  //此处可以对输入数据进行检验
+  pool.query('UPDATE xfn_category SET ? WHERE cid=?',[data,data.cid],(err,result)=>{
+    console.log(result);
+    if(err) throw err;
+    if(result.changedRows>0){
+      //实际真的更新了一行
+      res.send({code:200,msg:'1 categroy modified'})
+    }else if(result.affectedRows==0){
+      res.send({code:400,msg:' categroy modified not exist'})
+    }else if(result.affectedRows==1&& result.changedRows==0){
+      //影响到1行，但修改了0行---新值与旧值一样
+      res.send({code:401,msg:'1 categroy modified no modification'})
+    }
+    //res.send(
+      //,
+      //{code:400,msg:'0 categroy modified',not exists},
+      //
+    //)
+  })
+})
 
